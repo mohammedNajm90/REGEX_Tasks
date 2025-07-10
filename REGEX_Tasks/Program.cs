65cf2f34b478d30caea715d72b01f89dcb9ab1d8
Muthana-Ali <muthanaali@warithit.com>
2025-07-10 16:56:13 +0300

    feat: finished the first question.

--- REGEX_Tasks/Program.cs
+++ REGEX_Tasks/Program.cs
@@ -1,1 +1,8 @@
-﻿// السلام عليكم يا ابطال كل واحد يسوي برانش باسمه ويبدي يحل 
+﻿class Program
+{
+    static void Main()
+    {
+        // Question 1: Match all phone numbers in a local format like 0790xxxxxxx.
+        Console.WriteLine($"Question 1: {RegexFunctions.MatchPhonNumberLocalFormat("My number is 07901234567.")}");           // Output: 07901234567
+    }
+}
