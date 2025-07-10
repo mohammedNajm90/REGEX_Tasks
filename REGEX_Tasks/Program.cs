d8624c0ec494bfb2525a7f663e2e8f29586dc522
Mohammed <mohammed.najm.422@gmail.com>
2025-07-10 17:06:42 +0300

    add task1 and task2

--- REGEX_Tasks/Program.cs
+++ REGEX_Tasks/Program.cs
@@ -1,1 +1,15 @@
 ﻿// السلام عليكم يا ابطال كل واحد يسوي برانش باسمه ويبدي يحل 
+
+
+using REGEX_Tasks;
+
+Tasks task = new Tasks();
+
+
+//Console.WriteLine("Match all phone numbers in a local format " + task.MatchAllPhoneNumbers("My number is 07601234567"));
+List<string> res = new List<string>();
+res = task.ExtractEmails("Contact me at aliexample.com or supporthalal.io");
+foreach (string s in res)
+{
+    Console.WriteLine(s);
+}
---
+++ REGEX_Tasks/Tasks.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Reflection.Metadata.Ecma335;
+
+namespace REGEX_Tasks
+{
+    internal class Tasks
+    {
+        // داله اجيك بيها هل السلسله هي مو فارغه ولا نل
+        public bool checkStr(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return true;
+            return false;
+        }
+        //--------------------------------------------Task 1------------------------------------------
+        // دالة  استخراج أرقام الهواتف المحلية
+        public bool MatchAllPhoneNumbers(string inputText)
+        {
+            //اجيك النص المدخل مو نل او فارغ
+            if(checkStr(inputText))
+            {
+                return false;
+            }
+            // ارجع ترو اذا كان اسيا او عراقنا او كورك حيث يبدي بمفتاح الشركه مع 8 ارقام
+            return Regex.IsMatch(inputText, @"(079\d{8})|(077\d{8})|(075\d{8})") ? true : false;
+        }
+        //--------------------------------------------Task 2------------------------------------------
+        // دالة  استخراج البريد الإلكتروني
+        public List<string> ExtractEmails(string inputText)
+        {
+            //اجيك النص المدخل مو نل او فارغ
+            if (checkStr(inputText))
+            {
+                return new List<string>() {"Null Or Empty" };
+            }
+            List<string> result = new List<string>();  
+            // ارجع كل الانماط الي تشبة النمط مال اليميل
+            MatchCollection match = Regex.Matches(inputText, @"\w+@\w+\.\w+");
+            // اتاكد انه لقى شي يبدي يضيفهن بقائمه حتى ارجعها
+            if (match.Count > 0)
+            {
+                foreach (Match m in match)
+                {
+                    result.Add(m.Value);
+                }
+            }
+            else
+            {
+                // اذا لم يجد شي يرجع نص ماكو
+                result.Add("Not Found");
+            }
+            return result;
+        }
+    }
+}
