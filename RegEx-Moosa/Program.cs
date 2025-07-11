701f038337ead29b2ab85390f09ee5a2a15a165f
MoOsA5 <moosasumer01@gmail.com>
2025-07-11 17:53:24 +0300

    solved 10 tasks

---
+++ RegEx-Moosa/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RegEx_Moosa
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            List <String> phones = RegEx_Solutions.FindLocalPhoneNumbers("My number is 07901234567.");
+            Console.WriteLine(string.Join(", ", phones));
+
+            List<String> emails = RegEx_Solutions.FindallEmailAddresses("Contact me at ali@example.com or support@halal.io");
+            Console.WriteLine(string.Join(", ", emails));
+
+            List<String> words = RegEx_Solutions.FindallWordsStartWithCapitalLetter("Ali went to Basra and met Noor.");
+            Console.WriteLine(string.Join(", ", words));
+
+            List<String> numbers = RegEx_Solutions.FindallNumbers("I have 2 cats and 10 chickens");
+            Console.WriteLine(string.Join(", ", numbers));
+
+            List<String> hashtags = RegEx_Solutions.FindallHashtags("#blessed #codingLife");
+            Console.WriteLine(string.Join(", ", hashtags));
+
+            List<String> wordsEndWithIng = RegEx_Solutions.FindallWordsEndingWithIng("I am coding, running, and learning.");
+            Console.WriteLine(string.Join(", ", wordsEndWithIng));
+
+            List<String> amounts = RegEx_Solutions.FindallCurrencyAmounts(" I spent $100 on food and $29.99 on drinks.");
+            Console.WriteLine(string.Join(", ", amounts));
+
+            List<String> times = RegEx_Solutions.FindTime("The meeting is at 12:30.");
+            Console.WriteLine(string.Join(", ", times));
+
+            List<String> dates = RegEx_Solutions.FindDate("Today is 2025-07-10");
+            Console.WriteLine(string.Join(", ", dates));
+
+            List<String> wordsStartWithUn = RegEx_Solutions.FindallWordsstartWithUn(" unknown, unhappy, unlocked");
+            Console.WriteLine(string.Join(", ", wordsStartWithUn));
+
+        }
+    }
+}
---
+++ RegEx-Moosa/RegEx_Solutions.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace RegEx_Moosa
+{
+    class RegEx_Solutions
+    {
+        //Method to Match all phone numbers
+        public static List<String> FindLocalPhoneNumbers(String input)
+        {
+            try
+            {
+                //Safety check to check if input is null or whiteSpace
+                if (string.IsNullOrWhiteSpace(input))
+                    throw new ArgumentNullException(nameof(input), "Input is null or whitespace.");
+
+                MatchCollection matches = Regex.Matches(input, @"0790\d{7}");
+                List<string> phoneNumbers = new List<string>();
+
+                foreach (Match match in matches)
+                    phoneNumbers.Add(match.Value);
+
+                return phoneNumbers;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return new List<string>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<string>();
+            }
+        }
+
+        //Method to print all email addresses
+        public static List<String> FindallEmailAddresses(String input)
+        {
+            try
+            {
+                //Safety check to check if input is null or whiteSpace
+                if (string.IsNullOrWhiteSpace(input))
+                    throw new ArgumentNullException(nameof(input), "Input is null or whitespace.");
+
+                MatchCollection matches = Regex.Matches(input, @"[\w\.-]+@[\w\.-]+\.\w+");
+                List<String> emails = new List<string>();
+                foreach (Match match in matches)
+                    emails.Add(match.Value);
+
+                return emails;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return new List<string>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<string>();
+            }
+        }
+
+        //Method to print any word that starts with a capital letter
+        public static List <String> FindallWordsStartWithCapitalLetter(String input)
+        {
+            try
+            {
+                //Safety check to check if input is null or whiteSpace
+                if (string.IsNullOrWhiteSpace(input))
+                    throw new ArgumentNullException(nameof(input), "Input is null or whitespace.");
+
+                MatchCollection matches = Regex.Matches(input, @"\b[A-Z][a-z]*");
+                List<String> words = new List<String>();
+
+                foreach (Match match in matches)
+                    words.Add(match.Value);
+
+                return words;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return new List<String>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<String>();
+            }
+        }
+
+        //Method to print all integrs
+        public static List<String> FindallNumbers(String input)
+        {
+            try
+            {
+                //Safety check to check if input is null or whiteSpace
+                if (string.IsNullOrWhiteSpace(input))
+                    throw new ArgumentNullException(nameof(input), "Input is null or whitespace.");
+
+                MatchCollection matches = Regex.Matches(input, @"\b\d+\b");
+                List<String> numbers = new List<string>();
+
+                foreach (Match match in matches)
+                    numbers.Add(match.Value);
+                return numbers;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return new List<string>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<string>();
+            }
+        }
+
+        //method to print all hashtags
+        public static List <String> FindallHashtags(String input)
+        {
+            try
+            {
+                //Safety check to check if input is null or whiteSpace
+                if (string.IsNullOrWhiteSpace(input))
+                    throw new ArgumentNullException(nameof(input), "Input is null or whitespace.");
+
+                MatchCollection matches = Regex.Matches(input, @"#\w+");
+                List<String> hashtags = new List<string>();
+
+                foreach (Match match in matches)
+                    hashtags.Add(match.Value);
+
+                return hashtags;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return new List<string>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<string>();
+            }
+        }
+
+        //method to print all words that ends with ing
+        public static List <String> FindallWordsEndingWithIng (String input)
+        {
+            try
+            {
+                //Safety check to check if input is null or whiteSpace
+                if (string.IsNullOrWhiteSpace(input))
+                    throw new ArgumentNullException(nameof(input), "Input is null or whitespace.");
+
+                MatchCollection matches = Regex.Matches(input, @"\b\w+ing\b");
+                List<String> words = new List<string>();
+                foreach (Match match in matches)
+                    words.Add(match.Value);
+
+                return words;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return new List<string>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<string>();
+            }
+        }
+
+        //Method to print Find all currency amounts
+        public static List <String> FindallCurrencyAmounts (String input)
+        {
+            try
+            {
+                //Safety check to check if input is null or whiteSpace
+                if (string.IsNullOrWhiteSpace(input))
+                    throw new ArgumentNullException(nameof(input), "Input is null or whitespace.");
+
+                MatchCollection matches = Regex.Matches(input, @"\$\d+(\.\d{2})?");
+                List<String> amounts = new List<string>();
+
+                foreach (Match match in matches)
+                    amounts.Add(match.Value);
+
+                return amounts;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return new List<string>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<string>();
+            }
+        }
+
+        //Method to print time
+        public static List <String> FindTime (String input)
+        {
+            try
+            {
+                //Safety check to check if input is null or whiteSpace
+                if (string.IsNullOrWhiteSpace(input))
+                    throw new ArgumentNullException(nameof(input), "Input is null or whitespace.");
+
+                MatchCollection matches = Regex.Matches(input, @"\b\d{1,2}:\d{2}\b");
+                List<String> times = new List<string>();
+                foreach (Match match in matches)
+                    times.Add(match.Value);
+
+                return times;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return new List<string>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<string>();
+            }
+        }
+
+        //Method to print Date
+        public static List <String> FindDate (String input)
+        {
+            try
+            {
+                //Safety check to check if input is null or whiteSpace
+                if (string.IsNullOrWhiteSpace(input))
+                    throw new ArgumentNullException(nameof(input), "Input is null or whitespace.");
+
+                MatchCollection matches = Regex.Matches(input, @"\b\d{4}-\d{2}-\d{2}\b");
+                List<String> dates = new List<string>();
+
+                foreach (Match match in matches)
+                    dates.Add(match.Value);
+
+                return dates;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return new List<string>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<string>();
+            }
+        }
+
+        //Method to print any word that starts with "un"
+        public static List<String> FindallWordsstartWithUn (String input)
+        {
+            try
+            {
+                //Safety check to check if input is null or whiteSpace
+                if (string.IsNullOrWhiteSpace(input))
+                    throw new ArgumentNullException(nameof(input), "Input is null or whitespace.");
+
+                MatchCollection matches = Regex.Matches(input, @"un\w+");
+                List<String> words = new List<string>();
+
+                foreach (Match match in matches)
+                    words.Add(match.Value);
+
+                return words;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return new List<string>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<string>();
+            }
+        }
+    }
+}
